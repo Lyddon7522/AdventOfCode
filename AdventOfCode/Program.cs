5f7f4a9b26a9bcf787695a6e35d63a7249e205e7
mlyddon <lyddon.morg@gmail.com>
2022-12-01 23:03:21 -0600

    Final Solution and Tests

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        var inputPath = Path.Combine(Environment.CurrentDirectory, @"day01.txt");
 
+        var elves = ParseInput(inputPath);
+
+        Console.WriteLine(elves.Last());
+
+        Console.WriteLine(elves.Sum());
+
+        
+    }
+
+    public static List<int> ParseInput(string inputPath)
+    {
+        return File.ReadAllText(inputPath)
+            .Split("\n\n")
+            .Select(e =>
+                e.Split("\n")
+                    .Select(int.Parse)
+                    .Sum())
+            .Order()
+            .TakeLast(3)
+            .ToList();
+    }
+}
---
+++ AdventOfCodeTests/Day01.cs
@@ -0,0 +1,28 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace AdventOfCodeTests;
+
+public class Day01
+{
+    [Fact]
+    public void GivenFileWithElves_WhenSummed_ThenReturnElfWithMostCalories()
+    {
+        var inputPath = System.IO.Path.Combine(Environment.CurrentDirectory, @"day01test.txt");
+
+        var elves = Program.ParseInput(inputPath);
+
+        Assert.Equal(24000, elves.Last());
+
+    }
+
+    [Fact]
+    public void GivenFileWithElves_WhenSummed_ThenReturnTopThreeElvesWithMostCalories()
+    {
+        var inputPath = System.IO.Path.Combine(Environment.CurrentDirectory, @"day01test.txt");
+
+        var elves = Program.ParseInput(inputPath);
+
+        Assert.Equal(45000, elves.Sum());
+
+    }
+}
